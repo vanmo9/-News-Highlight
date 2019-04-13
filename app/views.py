cbcd8ae38c700d230e347c4f5d6401a47bd65514
mohassan <vanmowha@gmail.com>
2019-04-13 12:18:13 +0300

    dynamic routes

--- app/views.py
+++ app/views.py
@@ -1,11 +1,23 @@
 from flask import render_template
 from app import app
 
-# Views
+## Views
 @app.route('/')
 def index():
-
     '''
+
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    message = 'python the language'
+    return render_template('index.html',message = message)
+
+
+
+@app.route('/news/<int:news_id>') 
+def news(news_id):
+
+    '''
+    this is view function that returns the news details page and its data
+    '''
+    return render_template('news.html', id = news_id)
